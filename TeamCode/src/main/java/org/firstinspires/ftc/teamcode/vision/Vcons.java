34914410abc4ccefefc2416ecfc257f72595d445
Aamir-Home <ycds9242@gmail.com>
2026-07-11 15:32:43 -0400

    working on some stuff, testing using a supplier for pollenPose instead of a method.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Vcons.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Vcons.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode.vision;
 
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.hardware.limelightvision.LLResult;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.hardware.hackingHoundsHardware;
+
+import java.util.function.Supplier;
 
 public class Vcons {
     private hackingHoundsHardware robot = new hackingHoundsHardware();
@@ -12,6 +17,10 @@ public class Vcons {
     public double robX = robot.pinpoint.getPosX(DistanceUnit.INCH);
     public double robY = robot.pinpoint.getPosY(DistanceUnit.INCH);
 
+    double fieldX;
+    double fieldY;
+    double pollenX;
+    double pollenY;
     /** the follwing are just example values to demonstrate how the math would work **/
     double llheight = 8;
     double polheight = 1.45;
@@ -22,6 +31,7 @@ public class Vcons {
     double tx = result.getTx();
     double ty = result.getTy();
 
+    double heading = robot.pinpoint.getHeading(AngleUnit.RADIANS);
     double targetang = llangle + ty;
     double targetHeight = llheight - polheight;
 
@@ -31,13 +41,25 @@ public class Vcons {
     public double getPollenOffset(){
         return Math.tan(Math.toRadians(tx)) * getPollenDis();
     }
-    public double createPollenPose(){
+    public Pose createPollenPose(){
+        fieldX = (getPollenDis() * Math.cos(heading)) - (getPollenOffset() * Math.sin(heading));
+        fieldY = (getPollenDis() * Math.sin(heading)) + (getPollenOffset() * Math.cos(heading));
 
-        double pollenX = robX + getPollenOffset();
-        double pollen
-        new Pose2D()
+        pollenX = robX + fieldX;
+        pollenY = robY + fieldY;
+        return new Pose(pollenX, pollenY);
     }
 
+    Supplier<Pose> pollenPoseSupplier = () -> new Pose(
+            robX + fieldX,
+            robY + fieldY,
+            0
+    );
+
+    follower.followPath(new Path(new BezierLine(
+            new Point(follower.getPose()),
+            new Point(pollenPoseSupplier.get())
+            )));
 
     /** Todo - take distance and add to robPose for new coords and make path from one to other
      *  - after above is finished restructure into command that can be used in teleop withg variables in proper areas.**/
