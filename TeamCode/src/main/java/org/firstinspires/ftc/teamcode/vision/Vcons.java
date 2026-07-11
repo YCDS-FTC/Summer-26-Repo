89e1669871974d79377431d933b4b02443c006c5
Aamir-Home <ycds9242@gmail.com>
2026-07-11 12:18:03 -0400

    figure out how to create points that work with different robot orientations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Vcons.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Vcons.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.vision;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.hardware.hackingHoundsHardware;
 
 public class Vcons {
@@ -10,10 +13,32 @@ public class Vcons {
     public double robY = robot.pinpoint.getPosY(DistanceUnit.INCH);
 
     /** the follwing are just example values to demonstrate how the math would work **/
-    public double llheight = 8;
-    public double llangle = 25;
-    public double polheight = 1.45;
+    double llheight = 8;
+    double polheight = 1.45;
 
-    /** Todo - add in getting tx,ty from ll pipeline and distances + horizontal offsets
-    take distance and add to robPose for new coords and make path from one to other **/
+    double llangle = 25;
+
+    LLResult result = robot.limelight.getLatestResult();
+    double tx = result.getTx();
+    double ty = result.getTy();
+
+    double targetang = llangle + ty;
+    double targetHeight = llheight - polheight;
+
+    public double getPollenDis(){
+        return Math.tan(Math.toRadians(targetang)) * targetHeight;
+    }
+    public double getPollenOffset(){
+        return Math.tan(Math.toRadians(tx)) * getPollenDis();
+    }
+    public double createPollenPose(){
+
+        double pollenX = robX + getPollenOffset();
+        double pollen
+        new Pose2D()
+    }
+
+
+    /** Todo - take distance and add to robPose for new coords and make path from one to other
+     *  - after above is finished restructure into command that can be used in teleop withg variables in proper areas.**/
 }
