daaaf414bad08ce75db0bd6c8072f1cf609d9e3c
hmock <hmock@ycds.org>
2026-07-22 19:30:05 -0400

    Add Teleeop and Intake command
    Teleop file that is just copied from the Pedro docs
    
    Intake command just sets the power to the intake from a number

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.pedropathing.ivy.Command;
+
+import org.firstinspires.ftc.teamcode.hardware.hackingHoundsHardware;
+
+public class Intake {
+
+    private final hackingHoundsHardware robot;
+
+
+    public Intake(hackingHoundsHardware robot) {
+        this.robot = robot;
+    }
+
+
+    public Command setPower(double power) {
+        return Command.build()
+                .setExecute(() -> {
+                    robot.intake.setPower(power);
+                });
+
+
+    }
+
+}
