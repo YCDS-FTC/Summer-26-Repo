8af172358c3a0e796b3024599db06f1b762dbb56
hmock9242 <hmock@ycds.org>
2026-07-25 14:53:16 -0400

    Add Hood Mechanism

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Hood.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Mechanisms;
+
+import static org.firstinspires.ftc.teamcode.LookUpTables.hoodAngleLut;
+
+import org.firstinspires.ftc.teamcode.hardware.hackingHoundsHardware;
+
+import com.pedropathing.ivy.Command;
+import com.seattlesolvers.solverslib.util.InterpLUT;
+
+public class Hood {
+
+    private final hackingHoundsHardware robot;
+
+    public static double hoodAngle;
+
+
+    public Hood (hackingHoundsHardware robot){
+        this.robot = robot;
+
+    }
+
+    public Command setHoodAngle(double distanceToGoal){
+        return Command.build()
+                .setExecute(() -> {
+                    if (distanceToGoal > 200){
+                        hoodAngle = 0;
+                    } else if (distanceToGoal < -70) {
+                        hoodAngle = 0;
+                    } else{
+                        hoodAngle = hoodAngleLut.get(distanceToGoal);
+                    }
+                    robot.hood.setPosition(hoodAngle);
+                });
+    }
+
+}
