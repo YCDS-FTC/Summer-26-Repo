ff8c419a04e65e6080cf877902f45dcd7961a065
hmock9242 <hmock@ycds.org>
2026-07-24 21:10:51 -0400

    Add Led enum color table

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Led.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Led.java
@@ -8,6 +8,31 @@ public class Led {
 
     private final hackingHoundsHardware robot;
 
+    public enum Color {
+        OFF(0.00),
+        RED(0.279),
+        ORANGE(0.333),
+        YELLOW(0.388),
+        SAGE(0.444),
+        GREEN(0.5),
+        AZURE(0.555),
+        BLUE(0.611),
+        INDIGO(0.66),
+        VIOLET(0.721),
+        WHITE(1.0);
+
+        private final double value;
+
+        Color(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+    }
+
+
     public Led(hackingHoundsHardware robot){this.robot = robot;}
 
 
@@ -26,14 +51,24 @@ public class Led {
                 });
     };
 
+    public Command setShooterColor(Color color) {
+        return setShooterColor(color.getValue());
+    }
+
     /**
      * Read here for the color values for the lights <a href="https://cdn11.bigcommerce.com/s-x56mtydx1w/images/stencil/original/products/2275/15126/3118-0808-0002-Product-Insight-4__88285.1757516465.png?c=1" target="_blank">Example Here</a>
      * @param color  Sets the intake light to a color
      */
-    public Command setIntakeLight(double color){
+    public Command setIntakeColor(double color){
         return Command.build()
                 .setExecute(() -> {
                     robot.intakeLight.setPosition(color);
                 });
 
-    }}
+    }
+
+    public Command setIntakeColor(Color color) {
+        return setIntakeColor(color.getValue());
+    }
+
+}
