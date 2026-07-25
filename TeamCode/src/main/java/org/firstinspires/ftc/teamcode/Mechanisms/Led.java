9226b1738dc86d9402e6de42ac39fe2d7d3cdce2
hmock9242 <hmock@ycds.org>
2026-07-24 22:55:17 -0400

    Update Led documentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Led.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Led.java
@@ -51,6 +51,10 @@ public class Led {
                 });
     };
 
+    /**
+     * Read here for the color values for the lights <a href="https://cdn11.bigcommerce.com/s-x56mtydx1w/images/stencil/original/products/2275/15126/3118-0808-0002-Product-Insight-4__88285.1757516465.png?c=1" target="_blank">Example Here</a>
+     * @param color  Sets the shooter light to a color
+     */
     public Command setShooterColor(Color color) {
         return setShooterColor(color.getValue());
     }
@@ -67,6 +71,10 @@ public class Led {
 
     }
 
+    /**
+     * Read here for the color values for the lights <a href="https://cdn11.bigcommerce.com/s-x56mtydx1w/images/stencil/original/products/2275/15126/3118-0808-0002-Product-Insight-4__88285.1757516465.png?c=1" target="_blank">Example Here</a>
+     * @param color  Sets the intake light to a color
+     */
     public Command setIntakeColor(Color color) {
         return setIntakeColor(color.getValue());
     }
