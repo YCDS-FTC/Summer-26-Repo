5a313758d398ffb0bf92d886fe225019d83b810a
Aamir-Home <ar8.panther@gmail.com>
2026-07-28 22:01:02 -0400

    started on turret class with methods for everything + two extra methods for turret adjsuting., If wanted could add switchign pidf controllers for far so its better targeting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Turret.java
@@ -1,38 +1,83 @@
 package org.firstinspires.ftc.teamcode.Mechanisms;
 
+import static com.pedropathing.ivy.commands.Commands.infinite;
+
+import static org.firstinspires.ftc.teamcode.util.RobotConstants.closeCoefficients;
+import static org.firstinspires.ftc.teamcode.util.RobotConstants.farCoefficients;
+import static org.firstinspires.ftc.teamcode.util.RobotConstants.targetX;
+import static org.firstinspires.ftc.teamcode.util.RobotConstants.targetY;
+
 import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
 import com.pedropathing.ivy.Command;
+import com.seattlesolvers.solverslib.controller.PIDFController;
 
 import org.firstinspires.ftc.teamcode.hardware.hackingHoundsHardware;
+import org.firstinspires.ftc.teamcode.util.RobotConstants;
 
 public class Turret {
     private final hackingHoundsHardware robot;
-    private Follower follower;
+    private final Follower follower;
 
-    private boolean use_LL = false;
-    private boolean no_LL = true;
-    private boolean manual = false;
+    private boolean on = true;
+    private double dx;
+    private double dy;
+    private double goalHeading;
+    private double robHeading;
+
+
+    private final PIDFController closeTurretController = new PIDFController(closeCoefficients);
+    private final PIDFController farTurretController = new PIDFController(farCoefficients);
+
     public Turret(hackingHoundsHardware robot, Follower follower) {
         this.robot = robot;
         this.follower = follower;
     }
 
 
-    private double ticksperDegree = 6.45;
-    private double normA(double angle) {angle %= 360; if (angle < -180) angle += 360; else if (angle > 180) angle -= 360;return angle;}
+    private double normA(double angle) {
+        angle %= 360; if (angle < -180) angle += 360; else if (angle > 180) angle -= 360;
+        if(angle > 155){
+            angle = 155;
+        } else if(angle < -179){
+            angle = -179;
+        }
+        return angle;
+    }
+    public double getTurretPos(){
+        double ticksperDegree = 6.45;
+        return robot.turret.getCurrentPosition()/ ticksperDegree;
+    }
+
+    public double turretLeft(){
+       follower.setHeading(follower.getHeading() + 0.0261799);
+       return follower.getHeading();
+    }
+    public double turretRight(){
+        follower.setHeading(follower.getHeading() - 0.0261799);
+        return follower.getHeading();
+    }
+    public double makeTargetAngle(){
+        dx = follower.getPose().getX() - targetX;
+        dy = follower.getPose().getY() - targetY;
+        goalHeading = Math.toDegrees(Math.atan2(-dy, -dx));
+        robHeading = Math.toDegrees(follower.getHeading());
+        return normA(goalHeading - robHeading);
+    }
 
 
-
-    public Command
+    public Command periodic(){
+        return infinite(()->{
+            if(on){
+                closeTurretController.calculate(getTurretPos(), makeTargetAngle());
+            } else{
+                closeTurretController.calculate(getTurretPos(), getTurretPos());
+            }
+                }
+        );
+    }
 
     /**take all methods from previous repo and put into here. ALl act turretmath goes into single method with everything split into methods. Only thing which
      goes into periodic is method for setting turret power. Change between yesLL and noLL using llresult, seperateCommand thats runs infinitely for switching between states**/
 
-
-
-    public Command exampleCommand(double value) {
-        return Command.build()
-                .setExecute(() -> {
-                });
-    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotConstants.java
@@ -1,15 +1,13 @@
 package org.firstinspires.ftc.teamcode.util;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 public class RobotConstants {
     public static double gateOpen = .405;
     public static double gateClose = 0.44;
     public static double gateDurationMs = 1500;
 
-    public static double Turretkp = 0.03;
-    public static double Turretki = 0;
-    public static double Turretkd = 0.001;
-    public static double Turretkf = 0;
-
+    public static PIDFCoefficients closeCoefficients = new PIDFCoefficients(0.03, 0, 0.001, 0);
+    public static PIDFCoefficients farCoefficients = new PIDFCoefficients(0.011, 0, 0.00003,0);
     public static double TurretFarkp = 0.011;
     public static double TurretFarki = 0;
     public static double TurretFarkd = 0.00003;
@@ -19,4 +17,8 @@ public class RobotConstants {
     public static double Shooterki = 0;
     public static double Shooterkd = 0;
     public static double Shooterkf = 0.0003;
+
+    public static double targetX;
+    public static double targetY;
+
 }
