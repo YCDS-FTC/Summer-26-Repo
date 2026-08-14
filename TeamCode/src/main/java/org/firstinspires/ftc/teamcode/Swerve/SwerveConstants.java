034be05dcb961602f3ebb14ce05d5cc12af806b2
hmock9242 <hmock@ycds.org>
2026-08-14 19:11:16 -0400

    Add track dimensions and reference angles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/SwerveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/SwerveConstants.java
@@ -10,6 +10,14 @@ public final class SwerveConstants {
     public static double TurnKi = 0.0;
     public static double TurnKd = 0.0;
 
+    public static double TRACK_WIDTH = 16;
+    public static final double TRACK_LENGTH = 16;
+
+    //right front 45
+    //right back 135
+    //left back 225
+    //left front 315
+
     //point the wheel forward and put the angle you find in here it should make that the new forward or 0
     public static final double LEFT_FRONT_OFFSET_DEG = 0.0;
     public static final double LEFT_BACK_OFFSET_DEG = 0.0;
