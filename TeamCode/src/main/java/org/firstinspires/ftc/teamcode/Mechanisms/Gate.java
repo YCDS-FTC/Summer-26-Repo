71257091ece4422738861386ec5d19b4d06b724d
hmock9242 <hmock@ycds.org>
2026-08-15 00:35:16 -0400

    Gate commands are now instant commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Gate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Gate.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode.Mechanisms;
 
+import static com.pedropathing.ivy.commands.Commands.instant;
 import static com.pedropathing.ivy.commands.Commands.waitMs;
+import static com.pedropathing.ivy.groups.Groups.sequential;
 import static org.firstinspires.ftc.teamcode.util.RobotConstants.gateClose;
 import static org.firstinspires.ftc.teamcode.util.RobotConstants.gateDurationMs;
 import static org.firstinspires.ftc.teamcode.util.RobotConstants.gateOpen;
 
 import com.pedropathing.ivy.Command;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.hardware.hackingHoundsHardware;
 
@@ -19,39 +20,27 @@ public class Gate {
     }
 
     /**
-     * Opens the gate and closes it after a set duration (gateDurationMs)
+     * Opens the gate, waits gateDurationMs, then closes it.
      */
-    public Command cycleGate(){
-        ElapsedTime timer = null;
-        return Command.build()
-                .setExecute(() -> {
-                    robot.gate.setPosition(gateOpen);
-                    waitMs(gateDurationMs);
-                    robot.gate.setPosition(gateClose);
-                })
-                .setDone(() -> true);
+    public Command cycleGate() {
+        return sequential(
+                openGate(),
+                waitMs(gateDurationMs),
+                closeGate()
+        );
     }
 
     /**
      * Opens the gate
      */
-    public Command openGate(){
-        return Command.build()
-                .setExecute(() -> {
-                   robot.gate.setPosition(gateOpen);
-                })
-                .setDone(() -> true);
+    public Command openGate() {
+        return instant(() -> robot.gate.setPosition(gateOpen));
     }
 
     /**
      * Closes the gate
      */
-    public Command closeGate(){
-        return Command.build()
-                .setExecute(() -> {
-                   robot.gate.setPosition(gateClose);
-                })
-                .setDone(() -> true);
+    public Command closeGate() {
+        return instant(() -> robot.gate.setPosition(gateClose));
     }
-
 }
