efc0c64dac00f004ebcffbe69850af36c3104a76
hmock9242 <hmock@ycds.org>
2026-08-16 21:58:54 -0400

    Docs EncoderUtil

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/EncoderUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/EncoderUtil.java
@@ -9,10 +9,26 @@ public final class EncoderUtil {
 
     private static final double REF_VOLTAGE = 3.3;
 
+    /**
+     * Will only work with analog encoders and other encoders should be read other ways
+     * This will return the raw angle without any offsets applied when you tune the pods
+     * This is not recommended for general use
+     * This should be used for mostly debugging and testing
+     * @param encoder Pass in which ANALOG encoder you want to read against a 3.3 reference voltage
+     * @return
+     */
     public static double getEncoderAngle(AnalogInput encoder) {
         return encoder.getVoltage() / REF_VOLTAGE * 360.0;
     }
 
+    /**
+     * This will calculate the fastest way to get to the target angle
+     * See "delta" in setPod in SwerveDrivePod
+     * <a href="https://compendium.readthedocs.io/en/latest/tasks/drivetrains/swerve.html#:~:text=Directing%20the%20wheels">Reference Link</a>
+     * @param currentAngle Where you are
+     * @param targetAngle Where you want to be
+     * @return the shortest way to get between two angles
+     */
     public static double closestAngle(double currentAngle, double targetAngle) {
         // normalize both angles into 0-360, using true modulo, not Java's %
         currentAngle = modulo(currentAngle);
